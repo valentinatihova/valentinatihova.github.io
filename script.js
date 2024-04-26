69600ec0ee5b47f0d287486518a296559c25b500
Valentina Tihova <valentina.tihova@yandex.ru>
2024-04-26 10:58:18 +0300

    add dynamic filters

--- script.js
+++ script.js
@@ -23,3 +23,23 @@ function sendMail() {
     })
     .catch((err) => console.log(err));
 }
+// filters
+
+const filters = document.querySelectorAll('.filter-btn');
+
+filters.forEach(filterBtn => {
+    filterBtn.addEventListener('click', () => {
+        let id = filterBtn.getAttribute('id');
+        let projectCards = document.querySelectorAll('.project-card');
+        projectCards.forEach(card => {
+            if(card.getAttribute('data-tags').includes(id)){
+                card.classList.remove('hide');
+            } else{
+                card.classList.add('hide');
+            }
+        })
+
+        filters.forEach(btn => btn.classList.remove('active'));
+        filterBtn.classList.add('active');
+    })
+})
