a017d2bc8705403ee3e77d450eb27a39bdf522e8
Valentina Tihova <valentina.tihova@yandex.ru>
2024-05-02 09:34:25 +0300

    add multilang

---
+++ lang.js
@@ -0,0 +1,180 @@
+const LanguageDataArr = {
+  about: {
+    en: "About",
+    ru: "Обо мне",
+  },
+  skills: {
+    en: "Skills",
+    ru: "Навыки",
+  },
+  projects: {
+    en: "Projects",
+    ru: "Проекты",
+  },
+  "other-platforms": {
+    en: "Other platforms",
+    ru: "Другие платформы",
+  },
+  contact: {
+    en: "Contact",
+    ru: "Контакты",
+  },
+  contactinfo: {
+    en: "Contact Info",
+    ru: "Связаться со мной",
+  },
+  "get-to-know-more": {
+    en: "Get To Know More",
+    ru: "Немного",
+  },
+  "about-me": {
+    en: "About me",
+    ru: "Обо мне",
+  },
+  "about-me-detail_1": {
+    en: "No matter what projects I work on, data and its analysis come first. I am able to work with large volumes of information and solve complex and non-standard problems. I like to work in a team - it allows me to see the task from different points of view.",
+    ru: "В каких бы проектах не работала, на первое место выходят данные и их анализ. Умею работать с большими объемами информации, решать сложные и нестандартные задачи. Люблю работать в команде, - это позволяет видеть задачу с разных точек зрения.",
+  },
+  "about-me-detail_2": {
+    en: "I'm interest of Data Visualization and Data Art, some of my plots are used by customers. Looking for inspiration in Information designer such as Eleanor Lutz, Giorgia Lupi, Nadieh Bremer and many others.",
+    ru: "Я интересуюсь визуализацией данных и Дата Артом, некоторые из моих работ успешно испольузются клиентами. Вдохновляюсь работами таких дизайнеров как Элеонор Лутц, ДЖорджии Лупи, Нади Бремер и многими другими.",
+  },
+  "lng-here-are-my": {
+    en: "Here Are My",
+    ru: "Мои",
+  },
+  "lng-proficiencies": {
+    en: "Proficiencies",
+    ru: "Навыки",
+  },
+  "lng-proficiencies-summary": {
+    en: "I can optimize your data for better results, informed decisions and cost savings. My proficiency in technical tools like SQL, Python, HTML, CSS, Power BI and Tableau, combined with strong soft skills, enables me to deliver impactful data solutions.",
+    ru: "Я могу оптимизировать ваши данные для достижения лучших результатов, принятия обоснованных решений и экономии средств. Мое знание технических инструментов, таких как SQL, Python, HTML, CSS, Power BI и Tableau, в сочетании с сильными навыками межличностного общения позволяет мне предлагать эффективные решения для обработки данных и их визуализации.",
+  },
+  "data-analysis-header": {
+    en: "Data Analysis",
+    ru: "Анализ данных",
+  },
+  "data-analysis-detail": {
+    en: "Uncover hidden insights and patterns within your data to identify trends, anomalies and opportunities for data-driven decision-making.",
+    ru: "Раскройте скрытые идеи и закономерности в ваших данных, чтобы выявить тенденции, аномалии и возможности для принятия решений на основе данных.",
+  },
+  "data-analytics-header": {
+    en: "Data Analytics",
+    ru: "Аналитика данных",
+  },
+  "data-analytics-detail": {
+    en: "Harness the power of advanced analytics, including predictive models and machine learning solutions to forecast outcomes, optimize processes and gain a competitive edge.",
+    ru: "Используйте возможности расширенной аналитики, включая прогнозные модели и решения машинного обучения, для прогнозирования результатов, оптимизации процессов и получения конкурентного преимущества.",
+  },
+  "data-visualization-header": {
+    en: "Data Visualization",
+    ru: "Визуализация данных",
+  },
+  "data-visualization-detail": {
+    en: "Transform complex data into clear and compelling visual narratives. Create custom dashboards and reports for enhanced data communication and understanding.",
+    ru: "Преобразуйте сложные данные в четкие и убедительные визуальные повествования. Создавайте собственные информационные панели и отчеты для улучшения передачи и понимания данных.",
+  },
+  "web-development": {
+    en: "Web Development",
+    ru: "Веб разработка",
+  },
+  "web-development-detail": {
+    en: "Craft intuitive web interfaces that deliver information in an accessible format. Design and build data-driven web solutions, seamlessly blending functionality and aesthetics to present data solutions that engage and inform.",
+    ru: "Создавайте интуитивно понятные веб-интерфейсы, предоставляющие информацию в доступном формате. Проектируйте и создавайте веб-решения на основе данных, органично сочетая функциональность и эстетику для представления решений по работе с данными, которые привлекают и информируют.",
+  },
+  "back-development": {
+    en: "Backend Developmentt",
+    ru: "Бэк разработка",
+  },
+  "back-development-detail": {
+    en: "Collaborate seamlessly with your team to integrate data solutions into your business processes. Whether it's short-term projects or ongoing data-driven initiatives, I'm here to support your success.",
+    ru: "Беспрепятственно сотрудничайте со своей командой, чтобы интегрировать решения по обработке данных в свои бизнес-процессы. Будь то краткосрочные проекты или постоянные инициативы, основанные на данных, я здесь, чтобы поддержать ваш успех.",
+  },
+  "browse-through-my": {
+    en: "Browse Through Myt",
+    ru: "Некоторые из моих",
+  },
+  "projects": {
+    en: "Projects",
+    ru: "Проектов",
+  },
+  "projects-summary": {
+    en: "Some of my data analytics and data science projects based on Yandex.Practicum.",
+    ru: "Некоторые из моих проектов по дата-аналитике и ML на основе обучения в Яндекс.Практикуме.",
+  },
+  "btn-project": {
+    en: "View Project",
+    ru: "Посмотреть проект",
+  },
+  "project_subtitle_11": {
+    en: "Advertising company for computer games",
+    ru: "Развитие рекламной компании интернет-магазина по компьютерным играм",
+  },
+  "project_11": {
+    en: "Development of an advertising company for an online store for computer games, taking into account regional distribution.",
+    ru: "Выявление определяющие успешность игры закономерности. Позволяет сделать ставку на потенциально популярный продукт и спланировать рекламные кампании.",
+  },
+  "project_subtitle_10": {
+    en: "Сreating an automatic Tableau report",
+    ru: "Автоматизация отчетности в Tableau",
+  },
+  "project_10": {
+    en: "Tableau dashboard for Yandex.Zen.",
+    ru: "Создание аналитического отчета (дашборд) для Яндекс.Дзен в Tableau.",
+  },
+  "project_subtitle_9": {
+    en: "Advertising company for computer games",
+    ru: "Развитие рекламной компании интернет-магазина по компьютерным играм",
+  },
+  "project_9": {
+    en: "Development of an advertising company for an online store for computer games, taking into account regional distribution.",
+    ru: "Выявление определяющие успешность игры закономерности. Позволяет сделать ставку на потенциально популярный продукт и спланировать рекламные кампании.",
+  },
+  "project_subtitle_8": {
+    en: "Advertising company for computer games",
+    ru: "Развитие рекламной компании интернет-магазина по компьютерным играм",
+  },
+  "project_8": {
+    en: "Development of an advertising company for an online store for computer games, taking into account regional distribution.",
+    ru: "Выявление определяющие успешность игры закономерности. Позволяет сделать ставку на потенциально популярный продукт и спланировать рекламные кампании.",
+  },
+  "project_subtitle_7": {
+    en: "Advertising company for computer games",
+    ru: "Развитие рекламной компании интернет-магазина по компьютерным играм",
+  },
+  "project_7": {
+    en: "Development of an advertising company for an online store for computer games, taking into account regional distribution.",
+    ru: "Выявление определяющие успешность игры закономерности. Позволяет сделать ставку на потенциально популярный продукт и спланировать рекламные кампании.",
+  },
+  "project_subtitle_6": {
+    en: "Advertising company for computer games",
+    ru: "Развитие рекламной компании интернет-магазина по компьютерным играм",
+  },
+  "project_6": {
+    en: "Development of an advertising company for an online store for computer games, taking into account regional distribution.",
+    ru: "Выявление определяющие успешность игры закономерности. Позволяет сделать ставку на потенциально популярный продукт и спланировать рекламные кампании.",
+  },
+  "project_subtitle_5": {
+    en: "Advertising company for computer games",
+    ru: "Развитие рекламной компании интернет-магазина по компьютерным играм",
+  },
+  "project_5": {
+    en: "Development of an advertising company for an online store for computer games, taking into account regional distribution.",
+    ru: "Выявление определяющие успешность игры закономерности. Позволяет сделать ставку на потенциально популярный продукт и спланировать рекламные кампании.",
+  },
+};
+
+const langSwitcher = document.getElementById("langSwitcher");
+langSwitcher.addEventListener("change", changeLanguage);
+
+function changeLanguage() {
+  let currentLang = langSwitcher.value;
+
+  for (let key in LanguageDataArr) {
+    const elements = document.querySelectorAll("[class*=" + key + "]");
+    elements.forEach((element) => {
+      element.innerHTML = LanguageDataArr[key][currentLang];
+    });
+  }
+}
